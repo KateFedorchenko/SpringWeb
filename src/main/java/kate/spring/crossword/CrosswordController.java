b1c589f6166c234ae3df8f88b02cdf8ff2a36076
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2022-12-28 19:00:00 +0300

    CrossWord draft

--- src/main/java/kate/spring/crossword/CrosswordController.java
+++ src/main/java/kate/spring/crossword/CrosswordController.java
@@ -1,22 +1,90 @@
 package kate.spring.crossword;
 
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 @RestController
 public class CrosswordController {
+    private File file;
+    private static List<String> lines;
 
     /**
-     *
-     * @param s word that has letters and gaps (?), e.g. "a??l?" could be an "apple"
+     * @param str word that has letters and gaps (?), e.g. "a??l?" could be an "apple"
      * @return
      */
     @GetMapping("get-words")
-    public List<String> getWords(@RequestParam String s){
+    public List<String> getWords(@RequestBody String str) throws IOException {
+        if (str == null) {
+            throw new RuntimeException("No word was given.");
+        }
+        if (!checkWordLength(str) || !checkAstericsPresence(str)) {
+            throw new RemoteException("The word format is not allowed!");
+        }
+
+
         return null;
 
     }
+
+    private List<String> getMatchedList(String s) {
+        List<String> newList = new ArrayList<>();
+        for (String line : lines) {
+            if (s.length() == line.length()) {
+                newList.add(line);
+            }
+        }
+        return newList;
+    }
+
+    private void compileRegex(){
+
+    }
+
+
+    private boolean checkAstericsPresence(String str) {
+        char[] chars = str.toCharArray();
+        for (char c : chars) {
+            if (c == '*') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkWordLength(String str) throws IOException {
+        file = ResourceUtils.getFile("classpath:DBWords.txt");
+        generateWordList(file);
+        int max = lines.stream()
+                .map(String::length)
+                .max(Integer::compare)
+                .get();
+        if (str.length() > max) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private static List<String> generateWordList(File file) throws IOException {
+        try {
+            lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+            return lines;
+        } catch (IOException e) {
+            throw new IOException("Failed!");
+        }
+    }
+
 }

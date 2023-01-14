fcdb419f8809f159c678f8ef022fbdf69b0e3896
Ekaterina Matkovskaya <frau.fedorchenko2014@yandex.ru>
2023-01-14 10:41:17 +0300

    PurchaseService completed

--- src/main/java/kate/spring/purchase/PurchaseController.java
+++ src/main/java/kate/spring/purchase/PurchaseController.java
@@ -11,7 +11,6 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 public class PurchaseController {
-    private Map<String, List<ItemDTO>> shoppingCart = new ConcurrentHashMap<>();
     private PurchaseService purchaseService;
 
     public PurchaseController(PurchaseService purchaseService) {
@@ -20,44 +19,17 @@ public class PurchaseController {
 
     @GetMapping("get-shopping-cart")
     public List<ItemDTO> getItemListByBuyer(@RequestBody String buyerName) {
-        if (!shoppingCart.containsKey(buyerName)) {
-            throw new RuntimeException("No such buyer found");
-        }
-        return shoppingCart.get(buyerName);
+        return purchaseService.getItemListByBuyer(buyerName);
     }
 
     @PutMapping("add-buyer")
     public String addBuyer(@RequestBody String buyerName) {
-        if (shoppingCart.containsKey(buyerName)) {
-            throw new RuntimeException("Such buyer is already in the list");
-        }
-
-        shoppingCart.put(buyerName, new ArrayList<>());
-        return "New buyer added!";
+        return purchaseService.addBuyer(buyerName);
     }
 
     @PutMapping("add-item")
-    public void addItem(
-            @RequestBody ItemDTO itemDTO,
-            @RequestParam String buyerName
-    ) {
-        if (!shoppingCart.containsKey(buyerName)) {
-            throw new RuntimeException("Cannot add item as no such buyer exists");
-        }
-        List<ItemDTO> itemList = new CopyOnWriteArrayList<>(shoppingCart.get(buyerName));
-        int indexOfItem = getIndexOfItem(itemList, itemDTO.getItemName());
-
-        if (indexOfItem != -1) {
-            ItemDTO curItem = itemList.get(indexOfItem);
-
-            int newQuantity = curItem.getQuantity() + itemDTO.getQuantity();
-            BigDecimal newPrice = curItem.getPrice().add(itemDTO.getPrice()).divide(BigDecimal.valueOf(newQuantity));
-
-            itemList.get(indexOfItem).setQuantity(newQuantity);
-            itemList.get(indexOfItem).setPrice(newPrice);
-        } else {
-            itemList.add(itemDTO);
-        }
+    public void addItem(@RequestBody ItemDTO itemDTO, @RequestParam String buyerName) {
+        purchaseService.addItem(buyerName,itemDTO);
     }
 
     @DeleteMapping("remove-item")
@@ -67,19 +39,7 @@ public class PurchaseController {
 
     @GetMapping("get-all-shopping-cart")
     public Map<String, List<ItemDTO>> loadPurchaseList() {
-        return shoppingCart;
+        return purchaseService.loadPurchaseList();
     }
 
-    public int getIndexOfItem(List<ItemDTO> itemList, String itemName) {
-        for (ItemDTO item : itemList) {
-            if (item.getItemName().equals(itemName)) {
-                return itemList.indexOf(item);
-            }
-        }
-        return -1;
-    }
 }
-
-
-// 1) Controller only for Service request
-// 3)
--- src/main/java/kate/spring/purchase/PurchaseService.java
+++ src/main/java/kate/spring/purchase/PurchaseService.java
@@ -3,6 +3,7 @@ package kate.spring.purchase;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,7 +13,40 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class PurchaseService {
     private Map<String, List<ItemDTO>> shoppingCart = new ConcurrentHashMap<>();
 
+    public List<ItemDTO> getItemListByBuyer(String buyerName){
+        if (!shoppingCart.containsKey(buyerName)) {
+            throw new RuntimeException("No such buyer found");
+        }
+        return shoppingCart.get(buyerName);
+    }
+    public String addBuyer(String buyerName){
+        if (shoppingCart.containsKey(buyerName)) {
+            throw new RuntimeException("Such buyer is already in the list");
+        }
 
+        shoppingCart.put(buyerName, new ArrayList<>());
+        return "New buyer added!";
+    }
+    public void addItem(String buyerName, ItemDTO itemDTO){
+        if (!shoppingCart.containsKey(buyerName)) {
+            throw new RuntimeException("Cannot add item as no such buyer exists");
+        }
+        List<ItemDTO> itemList = new CopyOnWriteArrayList<>(shoppingCart.get(buyerName));
+        int indexOfItem = getIndexOfItem(itemList, itemDTO.getItemName());
+
+        if (indexOfItem != -1) {
+            ItemDTO curItem = itemList.get(indexOfItem);
+
+            int newQuantity = curItem.getQuantity() + itemDTO.getQuantity();
+            BigDecimal newPrice = curItem.getPrice().add(itemDTO.getPrice()).divide(BigDecimal.valueOf(newQuantity));
+
+            itemList.get(indexOfItem).setQuantity(newQuantity);
+            itemList.get(indexOfItem).setPrice(newPrice);
+        } else {
+            itemList.add(itemDTO);
+        }
+        shoppingCart.put(buyerName,itemList);
+    }
     public void removeItem(String buyerName, ItemDTO itemDTO){
         if (!shoppingCart.containsKey(buyerName)) {
             throw new RuntimeException("No such buyer found");
@@ -27,17 +61,20 @@ public class PurchaseService {
                 throw new RuntimeException("Failed! The inserted quantity is more than the item has now.");
             } else if (itemDTO.getQuantity() == currItem.getQuantity()) {
                 itemList.remove(currItem);
+            } else {
+                int newQuantity = currItem.getQuantity() - itemDTO.getQuantity();
+                BigDecimal newPrice = currItem.getPrice().divide(BigDecimal.valueOf(newQuantity));
+                itemList.get(indexOfItem).setQuantity(newQuantity);
+                itemList.get(indexOfItem).setPrice(newPrice);
             }
-
-            int newQuantity = currItem.getQuantity() - itemDTO.getQuantity();
-            BigDecimal newPrice = currItem.getPrice().divide(BigDecimal.valueOf(newQuantity));
-            itemList.get(indexOfItem).setQuantity(newQuantity);
-            itemList.get(indexOfItem).setPrice(newPrice);
         } else {
             throw new RuntimeException("Failed! No such item was added to the Cart earlier.");
         }
+        shoppingCart.put(buyerName,itemList);
     }
-
+    public Map<String, List<ItemDTO>> loadPurchaseList(){
+        return shoppingCart;
+    }
     private int getIndexOfItem(List<ItemDTO> itemList, String itemName) {
         for (ItemDTO item : itemList) {
             if (item.getItemName().equals(itemName)) {
@@ -46,6 +83,4 @@ public class PurchaseService {
         }
         return -1;
     }
-
-
 }
